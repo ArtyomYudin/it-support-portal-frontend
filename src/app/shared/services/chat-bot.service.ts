56ca414d6bc6e1a30b67fa829ee7251e56c8cb6b
Artyom Yudin <yudin.artyom@gmail.com>
2025-09-30 13:47:21 +0300

    fix chatbot mobile support

--- src/app/shared/services/chat-bot.service.ts
+++ src/app/shared/services/chat-bot.service.ts
@@ -15,8 +15,8 @@ export interface ChatMessage {
   providedIn: 'root'
 })
 export class ChatService {
-  private streamUrl = 'http://itsupport-center-inform.ru:8008/ask/stream';
-  private sseUrl = 'http://itsupport-center-inform.ru:8008/ask/sse';
+  private streamUrl = 'https://itsupport-center-inform.ru:8008/ask/stream';
+  private sseUrl = 'https://itsupport-center-inform.ru:8008/ask/sse';
 
   constructor(private http: HttpClient) {}
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   apiHost: 'itsupport-center-inform.ru',
   apiPort: 8888,
   apiTestURL: 'ws/',
-  apiUrl: 'http://itsupport-center-inform.ru:8888/api',
+  apiUrl: 'https://itsupport-center-inform.ru:8888/api',
   jwtLogin: 'token',
   jwtRefresh: 'token/refresh',
   WebSocketPath: 'api/ws'
