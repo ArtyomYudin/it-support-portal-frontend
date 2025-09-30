d6ce832f1047644b602bff0b82e50af15d2d1568
Artyom Yudin <yudin.artyom@gmail.com>
2025-09-30 16:40:03 +0300

    fix env

--- src/app/shared/services/chat-bot.service.ts
+++ src/app/shared/services/chat-bot.service.ts
@@ -18,7 +18,8 @@ export interface ChatMessage {
 export class ChatService {
   private streamUrl = '/ask/stream';
   private protocol = window.location.protocol === 'https:' ? 'https' : 'http';
-  private sseUrl  = `${this.protocol}://${environment.chatBotHost}${environment.chatBotAskSSEUrl}`;
+  // private sseUrl  = `${this.protocol}://${environment.chatBotHost}${environment.chatBotAskSSEUrl}`;
+  private sseUrl = `${environment.chatBotAskSSEUrl}`;
 
   constructor(private http: HttpClient) {}
 
